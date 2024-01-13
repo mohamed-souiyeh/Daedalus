2722a032c7a114eb1184edace4b23c3d59c3b5b6
mohamed-souiyeh <mohamedsouiyeh3@gmail.com>
2024-01-13 23:21:10 +0100

    started working on the control center functionality

--- src/configs/controlCenter.config.ts
+++ src/configs/controlCenter.config.ts
@@ -1,12 +1,27 @@
 
 
 export const mazeGenerationAlgorithms = {
-  RandomWalkDFS: "Random Walk DFS",
-  recursiveDevider: "recursive devider (wall adder)",
+  "RandomWalkDFS": {
+    name: "Random Walk DFS",
+    description: "Random Walk DFS is a maze generation algorithm that uses a depth-first search algorithm to generate a maze. It is a randomized version of the depth-first search algorithm. It is also known as the recursive backtracker algorithm. (for testing purposes only)",
+  },
+  "recursiveDevider": {
+    name: "recursive devider (wall adder)",
+    description: "The recursive devider algorithm is a maze generation algorithm that uses a recursive devider algorithm to generate a maze. It is a randomized version of the recursive devider algorithm. It is also known as the recursive devider algorithm. (for testing purposes only)",
+  },
 };
 
 export const mazeSolvingAlgorithms = {
-  BFS: "Breadth first search (BFS)",
-  Astar: "A* Algorithm",
-  Dijkstra: "Dijkstra's Algorithm",
+  "BFS": {
+    name: "Breadth first search (BFS)",
+    description: "Breadth-first search (BFS) is an algorithm for traversing or searching tree or graph data structures. It starts at the tree root (or some arbitrary node of a graph, sometimes referred to as a 'search key'[1]) and explores the neighbor nodes first, before moving to the next level neighbors. (for testing purposes only)",
+  },
+  "Astar": {
+    name: "A* Algorithm",
+    descreption: "A* is the most popular choice for pathfinding, because it’s fairly flexible and can be used in a wide range of contexts. A* is like Dijkstra’s Algorithm in that it can be used to find a shortest path. A* is like Greedy Best-First-Search in that it can use a heuristic to guide itself. (for testing purposes only)",
+  },
+  "Dijkstra": {
+    name: "Dijkstra's Algorithm",
+    descreption: "Dijkstra’s Algorithm is one of the more popular basic graph theory algorithms. It is used to find the shortest path between nodes on a graph. (for testing purposes only)",
+  },
 };
